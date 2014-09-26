0dca997879c693ca1f5621bc211fcca9f4a4e1a4
Artyom Eliseev <artemka.newmail@yandex.ru>
2014-09-26 23:53:28 +0400

    Setup gulp and tests.

--- moodcatcher/static/gulpfile.js
+++ moodcatcher/static/gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+		jasmine = require('gulp-jasmine');
 
 gulp.task('default', function () {
-
+	return gulp.src('src/specs/appSpecs.js')
+				.pipe(jasmine());
 });
