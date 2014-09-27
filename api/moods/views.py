0d9baa3ba7d8258ea1224be99f3c505ea99d0747
Anton Larkin <toxicwar94@yandex.ru>
2014-09-27 12:57:39 +0400

    Added category_id login in moods

--- api/moods/views.py
+++ api/moods/views.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from api.moods.serializers import MoodSerializer
-from moodcatcher.models import Mood
+from moodcatcher.models import Mood, MoodCategory
 from rest_framework import viewsets
 from rest_framework import permissions
+import random
 
 
 class MoodViewSet(viewsets.ModelViewSet):
@@ -16,3 +17,10 @@ class MoodViewSet(viewsets.ModelViewSet):
         obj.author = User.objects.get(pk=-1)
         obj.save()
         return super(MoodViewSet, self).post_save(obj, created)
+
+    def get_queryset(self):
+        if self.request.QUERY_PARAMS.has_key('category_id'):
+            category = MoodCategory.objects.filter(id=self.request.QUERY_PARAMS.get('category_id')).first()
+            moods = Mood.objects.filter(category=category) if category else None
+            return random.choice(moods) if moods else []
+        return super(MoodViewSet, self).get_queryset()
