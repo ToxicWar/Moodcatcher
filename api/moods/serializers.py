36d7daf41b7797132bed98677ef63c34f8b9eb83
Anton Larkin <toxicwar94@yandex.ru>
2014-09-27 07:13:57 +0400

    Improved image url in moods serializers

--- api/moods/serializers.py
+++ api/moods/serializers.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 from __future__ import unicode_literals
-from moodcatcher.models import Mood
+from django.contrib.sites.models import Site
 from rest_framework import serializers
+from moodcatcher.models import Mood
 
 
 class MoodSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.RelatedField()
+    image = serializers.SerializerMethodField('get_image')
     
     class Meta:
         model = Mood
@@ -14,3 +16,6 @@ class MoodSerializer(serializers.HyperlinkedModelSerializer):
                   'text',
                   'sound_media',
                   'posted')
+
+    def get_image(self, obj):
+        return 'http://localhost:8000/media/{}'.format(obj.image)
