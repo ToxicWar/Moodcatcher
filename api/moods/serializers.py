4d4ee1cae6b567fd1ef8fcc618236986f40b5c7a
Anton Larkin <toxicwar94@yandex.ru>
2014-09-27 14:02:58 +0400

    Added category in moods api

--- api/moods/serializers.py
+++ api/moods/serializers.py
@@ -1,16 +1,18 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from rest_framework import serializers
-from moodcatcher.models import Mood
+from moodcatcher.models import Mood, MoodCategory
 
 
 class MoodSerializer(serializers.HyperlinkedModelSerializer):
     author = serializers.RelatedField()
     image = serializers.SerializerMethodField('get_image')
-    
+    category = serializers.SerializerMethodField('get_category')
+
     class Meta:
         model = Mood
         fields = ('author',
+                  'category',
                   'image',
                   'text',
                   'sound_media',
@@ -24,3 +26,15 @@ class MoodSerializer(serializers.HyperlinkedModelSerializer):
         if obj.image:
             return 'http://localhost:8000/media/{}'.format(obj.image)
         return ''
+
+    def get_category(self, obj):
+        if self.context['request'] and self.context['request'].DATA.has_key('category'):
+            category_id = self.context['request'].DATA.get('category')
+            try:
+                query = {'pk': int(category_id)}
+            except ValueError as e:
+                query = {'name': category_id}
+            category = MoodCategory.objects.filter(**query).first()
+            obj.category = category
+            obj.save()
+        return obj.category
