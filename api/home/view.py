1a53097bfc72bffb71fb31e2502fb4a98726e391
Anton Larkin <toxicwar94@yandex.ru>
2014-09-28 13:36:28 +0400

    Added permission

--- api/home/view.py
+++ api/home/view.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from rest_framework import viewsets
+from rest_framework import permissions
 from moodcatcher.models import Mood
 from api.moods.serializers import MoodSerializer
 
@@ -10,4 +11,4 @@ DAYS_IN_MONTH = 30
 class HomeViewSet(viewsets.ModelViewSet):
     queryset = Mood.objects.order_by('-posted').all()[:DAYS_IN_MONTH]
     serializer_class = MoodSerializer
-    
+    permission_classes = (permissions.IsAuthenticated,)
