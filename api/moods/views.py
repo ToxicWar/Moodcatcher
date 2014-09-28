600764874d116ad72d0d7a10e23f99383eebd422
keipaglows <dark_inko@mail.ru>
2014-09-28 14:45:22 +0400

    get moods by category_id and get random mood by category_id

--- api/moods/views.py
+++ api/moods/views.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 from __future__ import unicode_literals
+
+import random
+
 from django.contrib.auth.models import User
 from api.moods.serializers import MoodSerializer
 from moodcatcher.models import Mood, MoodCategory
 from rest_framework import viewsets
 from rest_framework import permissions
-import random
 
 
 class MoodViewSet(viewsets.ModelViewSet):
@@ -24,21 +26,25 @@ class MoodViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         if self.request.QUERY_PARAMS.has_key('category_id'):
             category_id = self.request.QUERY_PARAMS.get('category_id')
-
+            
             try:
                 query = {'pk': int(category_id)}
             except ValueError as e:
                 query = {'name': category_id}
-
+                
             category = MoodCategory.objects.filter(**query).first()
-            moods = Mood.objects.filter(category=category) if category else None
-            mood = random.choice(moods) if moods else []
-            if mood:
-                if self.request.user.is_authenticated():
-                    mood.received.add(self.request.user)
-                else:
-                    mood.received.add(User.objects.get(pk=-1))
-                mood.save()
-                mood = [mood]
-            return mood
+            moods = Mood.objects.filter(category=category) if category else []
+            if self.request.QUERY_PARAMS.has_key('random'):
+                mood = random.choice(moods) if len(moods) else None
+                
+                if mood:
+                    if self.request.user.is_authenticated():
+                        mood.received.add(self.request.user)
+                    else:
+                        mood.received.add(User.objects.get(pk=-1))
+                    mood.save()
+                    mood = [mood]
+                    return mood
+                
+            return moods
         return super(MoodViewSet, self).get_queryset()
