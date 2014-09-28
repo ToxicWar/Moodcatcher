6776bd17ea922019de2e46e78e6355a8620ebfec
3bl3gamer <3bl3gamer@gmail.com>
2014-09-28 11:41:01 +0400

    A bit nerged

--- moodcatcher/static/src/js/app.controller.js
+++ moodcatcher/static/src/js/app.controller.js
@@ -1,7 +1,7 @@
 angular.module('moodcatcher')
 	.controller('AppController', function ($scope, $modal, $http, User, $rootScope, $location) {
-		$scope.currentUser = null;
-		User.me().then(function(u){ $scope.currentUser = u; });
+		$rootScope.currentUser = null;
+		User.me().then(function(u){ $rootScope.currentUser = u; console.log(u); $scope.$digest() });
 		$scope.user = new User({});
 		
 		function alertErr(err) {
